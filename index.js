e54e544fe168c497bf185504f5265373d93d3a63
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-12 18:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function logWhisper(string) {
   console.log('hello')
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  if ("hello") {
+    return "I can't hear you!"
+  else if ("HELLO") {
+    return "YES INDEED!"
+  else {
+    return "I love you, too."
+  }
+
+  }
+
+  }
+
+}
