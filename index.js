79276fbb6159c584758b678c1f26932b5852ab43
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-12 18:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,19 +17,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-
-  var uppercase = "HELLO!"
-   // true
-  var lowercase = 'hello!'
-   // true
-
-  if (lowercase.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  else if (uppercase.toUpperCase() === uppercase) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else if("I love you, Grandma") {
+  else if(string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
